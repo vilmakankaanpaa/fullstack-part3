44196c601253800a677f983f6c74c5062dd9c2f0
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-10-12 16:19:54 +0300

    Exercise 3.2

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-// let persons = [
-// 	{ 
-// 		id: 1,
-// 		name: "Arto Hellas", 
-// 		number: "040-123456"
-// 	},
-// 	{ 
-// 		id: 2,
-// 		name: "Ada Lovelace", 
-// 		number: "39-44-5323523"
-// 	},
-// 	{ 
-// 		id: 3,
-// 		name: "Dan Abramov", 
-// 		number: "12-43-234345"
-// 	},
-// 	{ 
-// 		id: 4,
-// 		name: "Mary Poppendieck", 
-// 		number: "39-23-6423122"
-// 	}
-// ]
-
 let persons = [
 	{ 
-		"id": 1,
-		"name": "Arto Hellas", 
-		"number": "040-123456"
+		id: 1,
+		name: "Arto Hellas", 
+		number: "040-123456"
 	},
 	{ 
-		"id": 2,
-		"name": "Ada Lovelace", 
-		"number": "39-44-5323523"
+		id: 2,
+		name: "Ada Lovelace", 
+		number: "39-44-5323523"
 	},
 	{ 
-		"id": 3,
-		"name": "Dan Abramov", 
-		"number": "12-43-234345"
+		id: 3,
+		name: "Dan Abramov", 
+		number: "12-43-234345"
 	},
 	{ 
-		"id": 4,
-		"name": "Mary Poppendieck", 
-		"number": "39-23-6423122"
+		id: 4,
+		name: "Mary Poppendieck", 
+		number: "39-23-6423122"
 	}
 ]
+
+app.get('/info', (request, response) => {
+	const info = 
+	`Phonebook has info for ${persons.length} people. ` +
+	`Current datetime is: ${new Date()}`
+	response.json(info)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
