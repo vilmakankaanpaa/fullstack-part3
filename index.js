1515d7f16b8ba6d8dcd05131b2c58eafc3dcf4a5
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-10-14 16:07:21 +0300

    Exercise 3.17

--- index.js
+++ index.js
@@ -81,6 +81,21 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 // Defining middleware that will not be used in the routes but will be
 // run if none of the routes handle the request
 const unknownEndpoint = (request, response) => {
