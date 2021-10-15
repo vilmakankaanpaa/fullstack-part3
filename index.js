5d4307eba1f04b46be476dbafa86565aa2dc04d3
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-10-15 08:41:30 +0300

    Exercise 3.19

--- index.js
+++ index.js
@@ -56,22 +56,8 @@ function assignPerson (req, res, next) {
   next()
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-	// if (body.name.length === 0) {
-  //   return response.status(400).json({ error: 'name missing' })
-  // }
-
-  // if (body.number.length === 0) {
-  //   return response.status(400).json({ error: 'number missing' })
-  // }
-
-	// if (persons.find(person => person.name === body.name)) {
-	// 	return response.status(400).json({ 
-  //     error: 'name must be unique' 
-  //   })
-	// }
 
   const person = new Person({
     name: body.name,
@@ -80,7 +66,7 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -111,7 +97,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+	} else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+	}
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 
@@ -13,9 +14,17 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+  number: {
+		type: String,
+		required: true
+	}
 })
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
